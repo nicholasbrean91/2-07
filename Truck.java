493d08a00958dc4ef046b35e0efd6938795c512d
nicholasbrean91 <31775539+nicholasbrean91@users.noreply.github.com>
2018-11-13 14:49:13 -0500

    Create Truck.java

---
+++ Truck.java
@@ -0,0 +1,15 @@
+
+public class Truck extends Vehicle {
+
+	public int numOfDoors;
+	
+	public Truck(String lPlateNum, 
+				 String colour, 
+				 String wheels) {
+		
+		super(lPlateNum, colour, wheels);
+		
+	}
+	
+	
+}
